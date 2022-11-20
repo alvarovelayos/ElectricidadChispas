e343b19cff8ccb7b459bfe9920f72e312c933485
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-20 19:16:01 +0100

    Subido clase sociedad

---
+++ src/Society.java
@@ -0,0 +1,75 @@
+public class Society {
+
+    public String clientCode;
+    public String bussinessName;
+    public String cif;
+    public String malingAddress;
+    public String town;
+    public String province;
+    public String email;
+    public String phone;
+
+    public String getClientCode() {
+        return clientCode;
+    }
+
+    public void setClientCode(String clientCode) {
+        this.clientCode = clientCode;
+    }
+
+    public String getBussinessName() {
+        return bussinessName;
+    }
+
+    public void setBussinessName(String bussinessName) {
+        this.bussinessName = bussinessName;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getMalingAddress() {
+        return malingAddress;
+    }
+
+    public void setMalingAddress(String malingAddress) {
+        this.malingAddress = malingAddress;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
