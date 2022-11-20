b276c050ce036ddbf09d591f4547ad9a2e73eba3
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-20 19:06:20 +0100

    Subido clase Autonomo

---
+++ src/Freelance.java
@@ -0,0 +1,84 @@
+public class Freelance {
+
+    public String dni;
+    public String ClientCode;
+    public String name;
+    public String surname;
+    public String mailingAddress;
+    public String town;
+    public String province;
+    public String email;
+    public String phone;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getClientCode() {
+        return ClientCode;
+    }
+
+    public void setClientCode(String clientCode) {
+        ClientCode = clientCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMailingAddress() {
+        return mailingAddress;
+    }
+
+    public void setMailingAddress(String mailingAddress) {
+        this.mailingAddress = mailingAddress;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
