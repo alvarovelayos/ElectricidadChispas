1242790f5e73d77d49b860b4e34f7b33811bf113
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-20 20:19:39 +0100

    Modificada clase service

---
+++ src/Service.java
@@ -0,0 +1,48 @@
+public class Service {
+
+    private String productService;
+    private String nam;
+    private String trademark;
+    private Double cost;
+    private Integer varType;
+
+    public String getProductService() {
+        return productService;
+    }
+
+    public void setProductService(String productService) {
+        this.productService = productService;
+    }
+
+    public String getNam() {
+        return nam;
+    }
+
+    public void setNam(String nam) {
+        this.nam = nam;
+    }
+
+    public String getTrademark() {
+        return trademark;
+    }
+
+    public void setTrademark(String trademark) {
+        this.trademark = trademark;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Integer getVarType() {
+        return varType;
+    }
+
+    public void setVarType(Integer varType) {
+        this.varType = varType;
+    }
+}
